ccb04ddecc0c7211b3dc4d4867f59c7408c87bcc
0xTeaTwo <0xteatwo@protonmail.com>
2021-06-20 16:44:03 +0900

    tweaks

--- contracts/InitialOffering/TXJPInitialOffering.sol
+++ contracts/InitialOffering/TXJPInitialOffering.sol
@@ -36,7 +36,7 @@ contract TXJPInitialOffering is Ownable {
     event Received(address indexed account, uint amount);
 
     uint public constant START = 1625799600; //1day = 86400
-    uint public constant END = START + 5 days + 12 hours;
+    uint public constant END = START + 5 days + 9 hours;
     uint public constant TOTAL_DISTRIBUTE_AMOUNT = 36000e8;
     uint public constant MINIMAL_PROVIDE_AMOUNT = 100 ether;
     uint public totalProvided = 0;
