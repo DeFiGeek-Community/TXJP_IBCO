85b0a702912b0b85aefc8e5a291b2a3b3d6947ca
T2 <t2_tide@hotmail.com>
2021-06-20 15:15:43 +0900

    update scheme of subgraph

--- graph/generated/schema.ts
+++ graph/generated/schema.ts
@@ -12,64 +12,6 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
-  constructor(id: string) {
-    super();
-    this.set("id", Value.fromString(id));
-  }
-
-  save(): void {
-    let id = this.get("id");
-    assert(id !== null, "Cannot save ExampleEntity entity without an ID");
-    assert(
-      id.kind == ValueKind.STRING,
-      "Cannot save ExampleEntity entity with non-string ID. " +
-        'Considering using .toHex() to convert the "id" to a string.'
-    );
-    store.set("ExampleEntity", id.toString(), this);
-  }
-
-  static load(id: string): ExampleEntity | null {
-    return store.get("ExampleEntity", id) as ExampleEntity | null;
-  }
-
-  get id(): string {
-    let value = this.get("id");
-    return value.toString();
-  }
-
-  set id(value: string) {
-    this.set("id", Value.fromString(value));
-  }
-
-  get count(): BigInt {
-    let value = this.get("count");
-    return value.toBigInt();
-  }
-
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
-  }
-
-  get account(): Bytes {
-    let value = this.get("account");
-    return value.toBytes();
-  }
-
-  set account(value: Bytes) {
-    this.set("account", Value.fromBytes(value));
-  }
-
-  get userShare(): BigInt {
-    let value = this.get("userShare");
-    return value.toBigInt();
-  }
-
-  set userShare(value: BigInt) {
-    this.set("userShare", Value.fromBigInt(value));
-  }
-}
-
 export class EventInfo extends Entity {
   constructor(id: string) {
     super();
@@ -144,6 +86,24 @@ export class EventInfo extends Entity {
   set totalProvided(value: BigInt) {
     this.set("totalProvided", Value.fromBigInt(value));
   }
+
+  get totalClaimed(): BigInt {
+    let value = this.get("totalClaimed");
+    return value.toBigInt();
+  }
+
+  set totalClaimed(value: BigInt) {
+    this.set("totalClaimed", Value.fromBigInt(value));
+  }
+
+  get balanceOfTXJP(): BigInt {
+    let value = this.get("balanceOfTXJP");
+    return value.toBigInt();
+  }
+
+  set balanceOfTXJP(value: BigInt) {
+    this.set("balanceOfTXJP", Value.fromBigInt(value));
+  }
 }
 
 export class PersonalBalance extends Entity {
@@ -183,5 +143,23 @@ export class PersonalBalance extends Entity {
 
   set amount(value: BigInt) {
     this.set("amount", Value.fromBigInt(value));
+  }
+
+  get claimed(): boolean {
+    let value = this.get("claimed");
+    return value.toBoolean();
+  }
+
+  set claimed(value: boolean) {
+    this.set("claimed", Value.fromBoolean(value));
+  }
+
+  get claimedTXJPAmount(): BigInt {
+    let value = this.get("claimedTXJPAmount");
+    return value.toBigInt();
+  }
+
+  set claimedTXJPAmount(value: BigInt) {
+    this.set("claimedTXJPAmount", Value.fromBigInt(value));
   }
 }
--- graph/src/mapping.ts
+++ graph/src/mapping.ts
@@ -2,65 +2,88 @@ import { BigInt } from "@graphprotocol/graph-ts"
 import {
   TXJPInitialOffering,
   Claimed,
-  OwnershipTransferred,
   Received
 } from "../generated/TXJPInitialOffering/TXJPInitialOffering"
-import { ExampleEntity, EventInfo, PersonalBalance } from "../generated/schema"
+import { EventInfo, PersonalBalance } from "../generated/schema"
 
 export function handleClaimed(event: Claimed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // // Entities can be loaded from the store using a string ID; this ID
+  // // needs to be unique across all entities of the same type
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // // Entities only exist after they have been saved to the store;
+  // // `null` checks allow to create entities on demand
+  // if (entity == null) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
+
+  // // BigInt and BigDecimal math are supported
+  // entity.count = BigInt.fromI32(1).plus(entity.count)
+
+  // // Entity fields can be set based on event parameters
+  // entity.account = event.params.account
+  // entity.userShare = event.params.userShare
+
+  // // Entities can be written to the store with `.save()`
+  // entity.save()
+
+  // // Note: If a handler doesn't require existing field values, it is faster
+  // // _not_ to load the entity from the store. Instead, create it fresh with
+  // // `new Entity(...)`, set the fields that should be updated and save the
+  // // entity back to the store. Fields that were not set or unset remain
+  // // unchanged, allowing for partial updates to be applied.
+
+  // // It is also possible to access smart contracts from mappings. For
+  // // example, the contract that has emitted the event can be connected to
+  // // with:
+  // //
+  // // let contract = Contract.bind(event.address)
+  // //
+  // // The following functions can then be called on this contract to access
+  // // state variables and other data:
+  // //
+  // // - contract.END(...)
+  // // - contract.MINIMAL_PROVIDE_AMOUNT(...)
+  // // - contract.START(...)
+  // // - contract.TOTAL_DISTRIBUTE_AMOUNT(...)
+  // // - contract.TXJP(...)
+  // // - contract.owner(...)
+  // // - contract.provided(...)
+  // // - contract.totalProvided(...)
+
+  // EventInfo
+  let eventId = event.transaction.to.toHex() // event contract address
+  let eventBalance = EventInfo.load(eventId)
+  if (eventBalance == null) {
+    // do nothing
+    return
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = BigInt.fromI32(1).plus(entity.count)
+  // update totalClaimed
+  eventBalance.totalClaimed = eventBalance.totalClaimed.plus(event.params.userShare)
+  eventBalance.save()
 
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.userShare = event.params.userShare
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  // PersonalBalance
+  let personalId = event.transaction.from.toHex()  // sender address
+  let personalBalance = PersonalBalance.load(personalId)
+  if (personalBalance == null) {
+    // do nothing
+    return
+  }
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.END(...)
-  // - contract.MINIMAL_PROVIDE_AMOUNT(...)
-  // - contract.START(...)
-  // - contract.TOTAL_DISTRIBUTE_AMOUNT(...)
-  // - contract.TXJP(...)
-  // - contract.owner(...)
-  // - contract.provided(...)
-  // - contract.totalProvided(...)
+  // record
+  personalBalance.claimed = true
+  personalBalance.claimedTXJPAmount = event.params.TXJPAmount
+  personalBalance.save()
 }
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
 export function handleReceived(event: Received): void {
   // EventInfo
-  let eventId = event.transaction.to.toHex()
+  let eventId = event.transaction.to.toHex() // event contract address
   let eventBalance = EventInfo.load(eventId)
   if (eventBalance == null) {
     // initialize
@@ -71,6 +94,7 @@ export function handleReceived(event: Received): void {
     eventBalance.MINIMAL_PROVIDE_AMOUNT = contract.MINIMAL_PROVIDE_AMOUNT()
     eventBalance.TOTAL_DISTRIBUTE_AMOUNT =  contract.TOTAL_DISTRIBUTE_AMOUNT()
     eventBalance.totalProvided = BigInt.fromI32(0)
+    eventBalance.totalClaimed = BigInt.fromI32(0)
   }
 
   // update totalProvided
@@ -79,12 +103,13 @@ export function handleReceived(event: Received): void {
 
 
   // PersonalBalance
-  let personalId = event.transaction.from.toHex()
+  let personalId = event.transaction.from.toHex()  // sender address
   let personalBalance = PersonalBalance.load(personalId)
   if (personalBalance == null) {
     // initialize
     personalBalance = new PersonalBalance(personalId)
     personalBalance.amount = BigInt.fromI32(0)
+    personalBalance.claimed = false
   }
 
   // update provided
